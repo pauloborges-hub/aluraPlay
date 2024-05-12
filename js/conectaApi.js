53e625c37fc21f1dd38082f5d8b3dc10db87ce85
Paulo Borges <victorborges296@hotmail.com>
2024-05-12 17:13:39 -0300

    tratamento de erros para inserção de vídeos

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -19,6 +19,11 @@ async function criaVideo(titulo, descricao, url, imagem) {
             imagem: imagem
         })
     });
+
+    if(!conexao.ok) {
+        throw new Error("Não foi possível enviar o vídeo");
+    }
+    
     const conexaoConvertida = await conexao.json();
     
     return conexaoConvertida;
